ba7886842c6753d92e6f84d0b9175f5cc26f1e00
Gustavo Iafelix <gustavoiafelix@gmail.com>
2021-11-07 20:10:50 -0300

    First Commit

---
+++ src/script.js
@@ -0,0 +1,14 @@
+function welcome() {
+    let MSG = window.document.querySelector('.welcomeMessage');
+    let date = new Date();
+    let hour = date.getHours();
+
+    if (hour > 5 && hour < 12)
+        MSG.innerHTML = 'Bom dia! &#x1F307;';
+    else if (hour >= 12 && hour < 18)
+        MSG.innerHTML = 'Boa tarde! &#1F306;';
+    else if (hour >= 18 && hour < 23)
+        MSG.innerHTML = 'Boa noite! &#127747;';
+    else
+        MSG.innerHTML = 'Boa madrugada! &#x1F30C;';
+}
